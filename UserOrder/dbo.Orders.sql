139e83c39ab0e1e6629c17385cb9af83089ac383
Taras Yuzkiv <yuzkv.taras1111@gmail.com>
2023-05-13 00:41:25 +0300

    Added two SQL Query

---
+++ UserOrder/dbo.Orders.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[Orders] (
+    [OrderID]          INT             IDENTITY (1, 1) NOT NULL,
+    [UserID]           INT             NOT NULL,
+    [OrderDate]        DATETIME2 (7)   NOT NULL,
+    [OrderCost]        DECIMAL (18, 2) NOT NULL,
+    [ItemsDescription] NVARCHAR (1000) NOT NULL,
+    [ShippingAddress]  NVARCHAR (1000) NOT NULL,
+    CONSTRAINT [PK_Orders] PRIMARY KEY CLUSTERED ([OrderID] ASC),
+    CONSTRAINT [FK_Orders_Users_UserID] FOREIGN KEY ([UserID]) REFERENCES [dbo].[Users] ([UserID]) ON DELETE CASCADE,
+	CONSTRAINT [CK_Orders_OrderCost] CHECK (OrderCost >= 0)
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_Orders_UserID]
+    ON [dbo].[Orders]([UserID] ASC);
+
---
+++ UserOrder/dbo.Users.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Users] (
+    [UserID]      INT           IDENTITY (1, 1) NOT NULL,
+    [Login]       NVARCHAR (20) NOT NULL,
+    [Password]    NVARCHAR (50) NOT NULL,
+    [FirstName]   NVARCHAR (40) NOT NULL,
+    [LastName]    NVARCHAR (40) NOT NULL,
+    [DateOfBirth] DATETIME2 (7) NOT NULL,
+    [Gender]      NVARCHAR (1)  NOT NULL CHECK (Gender IN ('M', 'F')),
+    CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED ([UserID] ASC),
+	CONSTRAINT [UQ_Users_Login] UNIQUE ([Login])
+);
+
